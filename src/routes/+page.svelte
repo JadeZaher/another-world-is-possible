e34a22f980c1b578b90f2b618d7dd4cd1a3e8817
Ahmed Zaher <82454482+JadeZaher@users.noreply.github.com>
2024-10-19 20:23:34 -0600

    assets and images and such.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
 	import { Youtube } from 'svelte-youtube-lite';
-	import synopsis from "$lib/images/awp-img1.png";
+	import synopsis1 from "$lib/images/awp-img1.png";
+	import synopsis2 from "$lib/images/awp-img4.png";
 	import bhScene1 from "$lib/images/awp-img2.png";
 	import bhScene2 from "$lib/images/awp-img3.png";
+	import wolf from "$lib/images/awp - harvey.png";
+	import communityleader from "$lib/images/awp - community leader.png";
     const videoId = 'T7Gr3dljEVE'; // Replace with your actual video ID  
 </script>
 
@@ -39,16 +42,34 @@
 			But when we fail to do that we can end up facing disasterous conflicts.
 		</p>
 		<img
-		src={synopsis}
+		src={synopsis1}
 		  alt="Behind the scenes 1"
 		  class="w-full h-auto rounded-lg mb-4 z-50"
 		/>
+		<div class="flex md:flex-row items-center flex-col w-full py-10">
+			<div class="md:pr-10 pr-0">
+				<h1 class=" font-black">Zachary Marlow.</h1>
+				<p class="text-lg leading-relaxed ">
+					<br/>
+					I'm a person making this film one day at a time with everyone I can find that believes that we be the change that makes another world possible.
+				</p>
+				<p class="text-lg leading-relaxed ">
+					<br/>
+				This site is a work in progress and things are in flux, but I will do my best to keep you guys posted on what we have going on.
+				</p>
+			</div>
+			<img
+			src={synopsis2}
+			  alt=""
+			  class="md:max-w-[50vw] w-full h-auto rounded-lg mb-4 z-50 md:pt-0 pt-20"
+			/>
+		</div>
 	  </div>
 	</section>
   
 	<section id="TheProject" class=" py-48 text-blue-950 z-50 bg-teal-500 h-full">
 	  <div class="container mx-auto px-4">
-		<h3 class="text-3xl font-black mb-12 text-right">The Project</h3>
+		<h3 id="TheProject" class="text-3xl font-black mb-12 text-right">The Project</h3>
 		<div class="grid grid-cols-1 gap-8">
 		  <div>
 			<img
@@ -56,8 +77,29 @@
 			alt="Behind the scenes 1"
 			class="w-full h-auto rounded-lg mb-4"
 			/>
-			<p class="font-bold py-2 pb-10 p-5 rounded-md text-white md:text-xl text-sm ">Embark on a cinematic journey unlike any other. Our documentary series intricately weaves together the most pressing social, ecological, and economic crises of our time, unveiling the SYSTEM that drives these challenges and hinders real change. But this isn’t just a critique; it’s a visionary roadmap toward a NEW system—one that aspires to save our world.
-				Prepare for a dynamic odyssey bursting with drama, excitement, and mind-blowing ideas. Filmed across 16 U.S. states and 3 continents, our series features vibrant characters from all walks of life—from passionate youth in revolt to esteemed professors, scientists, activists, and indigenous leaders. You'll hear from legends like Noam Chomsky and countless other prominent figures, all voicing the urgent need for radical change and showcasing real-world solutions that demonstrate a better future is within reach.
+			<p class="font-bold py-2 pb-10 p-5 rounded-md text-white md:text-xl text-sm ">This documentary series intricately weaves together the most pressing social, ecological, 
+				and economic crises of our time, unveiling the system that drives these challenges and hinders real change. 
+				But this isn’t just a critique; it’s a visionary roadmap toward a new system—one that aspires to save our world.
+				Get ready for some mind-blowing ideas. 
+				</p>
+				<p class="font-bold py-2 pb-10 p-5 rounded-md text-white md:text-xl text-sm ">This documentary series intricately weaves together the most pressing social, ecological, 
+				Filmed across 16 U.S. states and 3 continents, our series features vibrant characters from all walks of life—from passionate youth in revolt to esteemed professors, 
+				<img
+				src={wolf}
+				alt="Behind the scenes 1"
+				class="w-full h-auto rounded-lg mb-4"
+				/>
+				<p class="font-bold py-2 pb-10 p-5 rounded-md text-white md:text-xl text-sm">
+					scientists, activists, and indigenous leaders. 
+					You'll hear from legends like Noam Chomsky and countless other prominent figures, all voicing the urgent need for radical change and showcasing real-world solutions. 
+					That demonstrate a better future is within reach.
+				</p>
+					<img
+						src={communityleader}
+						alt="Behind the scenes 1"
+						class="w-full h-auto rounded-lg mb-4"
+					/>
+				<p class="font-bold py-2 pb-10 p-5 rounded-md text-black md:text-xl text-sm">
 				Through compelling storytelling, we explore our disconnection from nature, the historical roots of our current crises, and the possibilities and threats posed by technology. We shine a light on the various global movements advocating for change and rethinking everything—from currency and governance to food systems and societal organization. This series is a thrilling blend of dystopian narrative, action, and nature documentary, all while remaining deeply character-driven and hopeful.
 				Join us as we take viewers on a wild ride from bustling financial centers to ancient indigenous villages and into the innovative pockets of the future around the globe. This series is led by experts and driven by community voices, presenting a real-life adventure filled with optimistic solutions to humanity’s biggest challenges.
 				This is not just a film; it’s a planetary story that connects us all. With successive interconnected narratives, we aim to inspire a grassroots movement and actionable, nonviolent plans for a regenerative cooperative transition. Together, we can spark direct change and foster a vision of unity over division.
@@ -71,8 +113,8 @@
 				class="md:w-1/2 h-auto rounded-lg mb-4 object-cover m-4"
 			/>
 			<div class="my-6">
-				<p id="WhatImlookingforrightnow" class="font-black text-2xl"> What Im looking for right now.</p>
-				<p class="text-white font-bold pt-16 p-10 rounded-md bg-black bg-opacity-10 my-9 h-full md:text-xl text-sm">I’ve poured my heart and soul into this film, tackling complex systemic issues with nothing but determination and resourcefulness. With virtually no budget, I’ve captured over 20 terabytes of breathtaking footage and completed 80% of the series, all while facing countless adventures along the way.
+				<p id="WhatImlookingforrightnow" class="font-black text-2xl text-black"> What Im looking for right now.</p>
+				<p class="text-white pt-16 p-10 rounded-md bg-black bg-opacity-10 my-9 h-full md:text-xl text-sm">I’ve poured my heart and soul into this film, tackling complex systemic issues with nothing but determination and resourcefulness. With virtually no budget, I’ve captured over 20 terabytes of breathtaking footage and completed 80% of the series, all while facing countless adventures along the way.
 					Now, I’m reaching out for your support to take this project to the next level. I need a small amount of funding to shoot the final segments on a global scale, filming some of the world’s most impactful projects abroad. Your assistance in securing a few high-profile interviews would be invaluable—I’ve achieved incredible things without connections, so imagine what we could accomplish together!
 					Here’s what I need:
 					<br/> <br/>
