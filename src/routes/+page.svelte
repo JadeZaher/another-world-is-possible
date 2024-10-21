2232e799cbadff5aba50b876eb24e197ec3ee701
Ahmed Zaher <82454482+JadeZaher@users.noreply.github.com>
2024-10-20 18:28:28 -0600

    update style reduce color block use

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,6 @@
 				<h2 class="text-4xl sm:text-6xl py-6 font-bold mb-4 z-[2] pointer-events-none">
 					Another World is Possible
 				</h2>
-				<div clas=" w-screen"></div>
 				<div class=" rounded-[25px] overflow-hidden m-[10px] pointer-events-auto z-40">
 					<Youtube id={videoId} showTitle="{false}"  />
 				</div>
@@ -31,7 +30,7 @@
 
 	</section>
   
-	<section id="synopsis" class="py-24 text-[#ecf0f1] w-full bg-[#16a085] min-h-[80vh]">
+	<section id="synopsis" class="py-24 text-black w-full bg-[#ffffff] min-h-[80vh]">
 	  <div class="container mx-auto px-4">
 		<h3 class="text-3xl font-bold mb-8">Synopsis</h3>
 		<p class="text-lg leading-relaxed py-10">
@@ -42,6 +41,7 @@
 			When we come together in the right ways for the right reasons we can do all goods and undo all evils.
 			But when we fail to do that we can end up facing disasterous conflicts.
 		</p>
+		<div class="w-full  border-t-2 border-teal-400 rounded-t-md"></div>
 		<img
 		src={synopsis1}
 		  alt="Behind the scenes 1"
@@ -68,7 +68,7 @@
 	  </div>
 	</section>
   
-	<section id="TheProject" class=" py-48 text-[#ecf0f1] z-50 bg-[#27ae60] h-full">
+	<section id="TheProject" class=" py-48 text-[#000000] z-50 bg-white h-full rounded-t-md shadow-lg border-teal-200 border-t-2">
 	  <div class="container mx-auto px-4">
 		<h3 id="TheProject" class="text-3xl font-black mb-12 text-right">The Project</h3>
 		<div class="grid grid-cols-1 gap-8 py-5">
@@ -78,12 +78,12 @@
 			alt="Behind the scenes 1"
 			class="w-full h-auto rounded-lg mb-4"
 			/>
-			<p class="font-bold py-4 pb-10 p-5 rounded-md text-[#ecf0f1] md:text-xl text-sm ">This documentary series intricately weaves together the most pressing social, ecological, 
+			<p class="font-bold py-4 pb-10 p-5 rounded-md text-black md:text-xl text-sm ">This documentary series intricately weaves together the most pressing social, ecological, 
 				and economic crises of our time, unveiling the system that drives these challenges and hinders real change. 
 				But this isn’t just a critique; it’s a visionary roadmap toward a new system—one that aspires to save our world.
 				Get ready for some mind-blowing ideas. 
 				</p>
-				<p class="font-bold py-10 pb-10 p-5 rounded-mdtext-[#ecf0f1] md:text-xl text-sm ">This documentary series intricately weaves together the most pressing social, ecological, 
+				<p class="font-bold py-10 pb-10 p-5 rounded-md text-black md:text-xl text-sm ">This documentary series intricately weaves together the most pressing social, ecological, 
 				Filmed across 16 U.S. states and 3 continents, our series features vibrant characters from all walks of life—from passionate youth in revolt to esteemed professors, 
 				</p>
 				<img
@@ -91,7 +91,7 @@
 				alt="Behind the scenes 1"
 				class="w-full h-auto rounded-lg mb-4"
 				/>
-				<p class="font-bold py-10 pb-10 p-5 rounded-md text-white md:text-xl text-sm">
+				<p class="font-bold py-10 pb-10 p-5 rounded-md text-black md:text-xl text-sm">
 					scientists, activists, and indigenous leaders. 
 					You'll hear from legends like Noam Chomsky and countless other prominent figures, all voicing the urgent need for radical change and showcasing real-world solutions. 
 					That demonstrate a better future is within reach.
@@ -101,7 +101,7 @@
 						alt="Behind the scenes 1"
 						class="w-full h-auto rounded-lg mb-4"
 					/>
-				<p class="font-bold py-10 pb-10 p-5 rounded-md text-[#ecf0f1] md:text-xl text-sm">
+				<p class="font-bold py-10 pb-10 p-5 rounded-md text-black md:text-xl text-sm">
 				Through compelling storytelling, we explore our disconnection from nature, the historical roots of our current crises, and the possibilities and threats posed by technology. We shine a light on the various global movements advocating for change and rethinking everything—from currency and governance to food systems and societal organization. This series is a thrilling blend of dystopian narrative, action, and nature documentary, all while remaining deeply character-driven and hopeful.
 				Join us as we take viewers on a wild ride from bustling financial centers to ancient indigenous villages and into the innovative pockets of the future around the globe. This series is led by experts and driven by community voices, presenting a real-life adventure filled with optimistic solutions to humanity’s biggest challenges.
 				This is not just a film; it’s a planetary story that connects us all. With successive interconnected narratives, we aim to inspire a grassroots movement and actionable, nonviolent plans for a regenerative cooperative transition. Together, we can spark direct change and foster a vision of unity over division.
@@ -115,8 +115,8 @@
 				class="md:w-1/2 h-auto rounded-lg mb-4 object-cover m-4"
 			/>
 			<div class="my-6">
-				<p id="WhatImlookingforrightnow" class="font-black text-2xl text-[#ecf0f1] md:pl-8 pl-0"> What Im looking for right now.</p>
-				<p class="text-[#ecf0f1] pt-16 p-10 rounded-md my-9 h-full md:text-xl text-sm">I’ve poured my heart and soul into this film, tackling complex systemic issues with nothing but determination and resourcefulness. With virtually no budget, I’ve captured over 20 terabytes of breathtaking footage and completed 80% of the series, all while facing countless adventures along the way.
+				<p id="WhatImlookingforrightnow" class="font-black text-2xl text-black md:pl-8 pl-0"> What Im looking for right now.</p>
+				<p class="text-black pt-16 p-10 rounded-md my-9 h-full md:text-xl text-sm">I’ve poured my heart and soul into this film, tackling complex systemic issues with nothing but determination and resourcefulness. With virtually no budget, I’ve captured over 20 terabytes of breathtaking footage and completed 80% of the series, all while facing countless adventures along the way.
 					Now, I’m reaching out for your support to take this project to the next level. I need a small amount of funding to shoot the final segments on a global scale, filming some of the world’s most impactful projects abroad. Your assistance in securing a few high-profile interviews would be invaluable—I’ve achieved incredible things without connections, so imagine what we could accomplish together!
 					Here’s what I need:
 					<br/> <br/>
@@ -131,3 +131,9 @@
 	</section>
   
 </div>
+
+<style lang="postcss" scoped>
+	img{
+		@apply rounded-md shadow-2xl;
+	}
+</style>
